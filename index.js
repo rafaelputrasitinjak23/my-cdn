325b8a2ccab319b8ecc224290a337eeff9364b2c
Rafaelputera <rafaelputrasitinjak76@gmail.com>
2026-05-03 19:10:03 +0700

    Add route to serve index.html

--- index.js
+++ index.js
@@ -163,6 +163,10 @@ app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/files', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'files.html'));
 });
